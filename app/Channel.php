776b76d3fad5824a93f5b0f68b606cc6651228aa
Ali Veseli <ali.veseli.93@gmail.com>
2019-08-17 13:02:38 +0200

    2.1 - Channel  Created & Registration Form Updated

---
+++ app/Channel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Channel extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug', 'description','image_filename'
+    ];
+
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+}
